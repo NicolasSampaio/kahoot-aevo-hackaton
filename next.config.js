48397c2235bf373886dc9fe43223ab768724ebad
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2026-03-20 17:35:57 -0300

    vercel build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Ensure all API routes are included in the build
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
